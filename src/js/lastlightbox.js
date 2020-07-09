da8ecbeb4dc85af4769176f7096f8312d940b52f
Mario Zoth <mario.zoth@icloud.com>
2020-07-09 15:16:05 +0200

    Add basic media selection

--- src/js/lastlightbox.js
+++ src/js/lastlightbox.js
@@ -19,10 +19,20 @@ class LastLightbox {
     }
 
     /**
+     * Find media elements in DOM
+     */
+    selectMedia() {
+        console.info(`${this.logPrefix} Searching for media…`)
+        return document.querySelectorAll('main img')
+    }
+
+    /**
      * Initialize lightbox
      */
     initialize() {
         console.info(`${this.logPrefix} Initializing…`)
+        const media = this.selectMedia()
+        console.info(`${this.logPrefix} Found ${!!media && media.length || 0} media elements.`)
     }
 }
 
