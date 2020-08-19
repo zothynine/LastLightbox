e8c34741f8ef7759dfd9cfe6c052d90eddaab654
Mario Zoth <mario.zoth@icloud.com>
2020-08-19 20:18:08 +0200

    Use lightbox src if provided

--- src/js/slides.js
+++ src/js/slides.js
@@ -7,6 +7,7 @@ function create() {
     this.media.forEach(item => {
         const clone = item.cloneNode()
         clone.removeAttribute('class')
+        !!clone.dataset.llsrc && clone.setAttribute('src', clone.dataset.llsrc)
         const figureStr = clone.outerHTML
         this.slidesList.insertAdjacentHTML('beforeend', `
         <li class="ll--slide">
